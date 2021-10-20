012c4f55c2c7bf04f2592137052cd1ad06121d87
rex-woods43 <jessica.woods43@education.nsw.gov.au>
2021-10-20 12:42:23 +1100

    completed

--- 05forloop.py
+++ 05forloop.py
@@ -1,2 +1,9 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    05HelloWorld                                |")
+print("|    Name : Rex Woods                            |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
 for number in range(1,500):
     print(number)
