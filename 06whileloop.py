19f01b21d04c664d371353f0161ae6ea703580c1
rex-woods43 <jessica.woods43@education.nsw.gov.au>
2021-10-26 09:20:33 +1100

    completed

---
+++ 06whileloop.py
@@ -0,0 +1,13 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    06 whileLoop                                |")
+print("|    Name : Rex Woods                            |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+subject = input("What is the name of this subject ")
+while subject != "IST":
+    print ("Not Correct - try again")
+    subject = input("What is the name of this subject ")
+    if subject == "IST": 
+       print("\n\n Congratulations!!")
