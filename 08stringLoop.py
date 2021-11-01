ba83e8a5cde01f8952338f177800279112cb35be
rex-woods43 <jessica.woods43@education.nsw.gov.au>
2021-11-02 10:59:38 +1100

    completed

---
+++ 08stringLoop.py
@@ -0,0 +1,11 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    08StringLoop                                |")
+print("|    Name : Rex Woods                            |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+string = input("What is your string? ")
+for element in string:
+    print(element)
+
