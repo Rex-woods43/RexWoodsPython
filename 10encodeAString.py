b4e0943e73367d79a62707cf0c5127a60104dfa4
rex-woods43 <jessica.woods43@education.nsw.gov.au>
2021-11-02 17:00:20 +1100

    completed

---
+++ 10encodeAString.py
@@ -0,0 +1,14 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    10EncodeAString                             |")
+print("|    Name : Rex Woods                            |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+string = input("What is your string? ")
+encoded_message = ""
+for element in string:
+    encoded_element = chr(ord(element)+1)
+    print(element, "=", encoded_element)
+    encoded_message += encoded_element
+    print(encoded_message)
